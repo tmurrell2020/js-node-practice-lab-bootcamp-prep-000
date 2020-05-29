9fe8a0660d75219692f116e2438472465e98f91e
Terrell Murrell <littlerell12@gmail.com>
2020-05-29 23:13:04 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Terrell"
-var height = 74
+var height = "74"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height+5)
+console.log(parseInt(height)+5)
